2ddb00c9af823ba598184d943fedb4c9eae630e6
Amit binu <binua@mcmaster.ca>
2017-07-20 21:58:41 -0400

    Modified to not run the this js file until the user logs in

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
-window.onload = function(){
-	start = document.getElementById("start");
-	stop = document.getElementById("stop");
-	time = document.getElementById("time");
 
-	
-	if(start.addEventListener){
-		start.addEventListener("click",startListening);
-	}
 
-	if(stop.addEventListener){
-		stop.addEventListener("click", stopListening);
-	}
+var checkKeyWord = function(){
+	if(userKeyWord.value == ""){
+			time.innerHTML = "Please enter a Keyword";
+			time.style.fontSize = '3em';
+			time.style.visibility = 'visible';
+			userKeyWord.style.border = '0.09em solid red';
+		}
+		else{
+			userKeyWord.style.border = '';
+			time.innerHTML = "Listening ...";
+			time.style.fontSize = '4em';
+			time.style.visibility = 'visible';
+			startListening();
+		}
 };
 
 var startListening = function(){
-	time.style.visibility = 'visible';
 	var recognition = new webkitSpeechRecognition();
 	recognition.continuous = true;
 	recognition.interimResults = true;
@@ -22,9 +24,12 @@ var startListening = function(){
   		for (var i = event.resultIndex;i < event.results.length; ++i) {
   			if(event.results[i].isFinal){
   				var text = event.results[i][0].transcript;
-  				
-  				if(text == 'get ready'){
-  					console.log(event.results[i][0].transcript);
+  				text = text.toLowerCase().trim();
+  				userText = userKeyWord.value.toLowerCase().trim();
+  				console.log(text);
+  				if(text.includes(userText )){
+  					//console.log(userText);
+  					alert(userText);
   					//push the notification
   				}
   			}
